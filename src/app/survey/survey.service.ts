38b41148544f57f6f16475a0c6af010a3ddcec9f
MarvinKweyu <mkweyu1@gmail.com>
2021-07-07 12:55:06 +0300

    chore: catch errors not returned in backend API

--- src/app/survey/survey.service.ts
+++ src/app/survey/survey.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {AuthService} from "../auth/auth.service";
+import {throwError} from "rxjs";
+import {catchError} from "rxjs/operators";
+import {NotificationService} from "../shared/notification.service";
 
 @Injectable({
   providedIn: 'root'
@@ -19,9 +22,6 @@ export class SurveyService {
 
 
   getSurvey (): any{
-    const httpHeaders: HttpHeaders = new HttpHeaders({
-      Authorization: `Bearer HERfHpjCqUqKd22IwmA5F759ytnZMS`
-    });
     return this.http.get(this.busaraSurvey,this.authService.getHeaders())
   }
 
@@ -30,7 +30,21 @@ export class SurveyService {
   }
 
   submitSurvey(surveyData: any){
-    console.log('data submitting', surveyData);
     return this.http.post(this.busaraSurveyAnswers, surveyData,this.authService.getHeaders())
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  handleError(err: { error: { errors: any; Error: any; }; status: any; }) {
+    // * catch errors not returned in the response of the service
+    let errorMessage: string;
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.Error}`;
+    } else {
+      errorMessage = `Backend returned code ${err.status}: ${err.error.errors[0].errors[0]}`;
+    }
+    alert(errorMessage)
+    return throwError(errorMessage);
   }
 }
